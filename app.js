76807b6b0da5bfc40e6289601594404d8063c4a6
tina1998612 <tina26919742@gmail.com>
2017-10-14 23:38:40 -0400

    ipfs html ok

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+const IPFS = require('ipfs')
+const node = new IPFS()
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
@@ -12,3 +14,13 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
     console.log('App listening on port 3000!');
 });
+
+node.on('ready', () => {
+    // Your node is now ready to use \o/
+    console.log('ready');
+
+    // stopping a node
+    node.stop(() => {
+        // node is now 'offline'
+    })
+})
